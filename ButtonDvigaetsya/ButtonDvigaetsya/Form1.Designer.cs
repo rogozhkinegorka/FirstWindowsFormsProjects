0632a6ad152a660fe519b1c75b200b4abe85a7e4
rogozhkinegorka <rogozhkinegorka@gmail.com>
2020-05-25 01:29:27 +0300

    ok

---
+++ ButtonDvigaetsya/ButtonDvigaetsya/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace ButtonDvigaetsya
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonA = new System.Windows.Forms.Button();
+            this.buttonB = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonA
+            // 
+            this.buttonA.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.buttonA.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.buttonA.Location = new System.Drawing.Point(227, 204);
+            this.buttonA.Name = "buttonA";
+            this.buttonA.Size = new System.Drawing.Size(104, 41);
+            this.buttonA.TabIndex = 0;
+            this.buttonA.Text = "A";
+            this.buttonA.UseVisualStyleBackColor = false;
+            this.buttonA.Click += new System.EventHandler(this.buttonA_Click);
+            // 
+            // buttonB
+            // 
+            this.buttonB.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.buttonB.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.buttonB.Location = new System.Drawing.Point(457, 204);
+            this.buttonB.Name = "buttonB";
+            this.buttonB.Size = new System.Drawing.Size(104, 41);
+            this.buttonB.TabIndex = 2;
+            this.buttonB.Text = "B";
+            this.buttonB.UseVisualStyleBackColor = false;
+            this.buttonB.Click += new System.EventHandler(this.buttonB_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonB);
+            this.Controls.Add(this.buttonA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonA;
+        private System.Windows.Forms.Button buttonB;
+    }
+}
+
---
+++ ButtonDvigaetsya/ButtonDvigaetsya/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ButtonDvigaetsya
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonA_Click(object sender, EventArgs e)
+        {
+            if (buttonB.Right - buttonB.Width - buttonA.Right >= 20)
+            {
+                buttonA.Left += 10;
+                buttonB.Left -= 10;
+            }
+            else if (buttonB.Right - buttonB.Width - buttonA.Right > 0)
+            {
+                buttonA.Left += (buttonB.Right - buttonB.Width - buttonA.Right) / 2;
+                buttonB.Left -= (buttonB.Right - buttonB.Width - buttonA.Right);
+            }
+        }
+
+        private void buttonB_Click(object sender, EventArgs e)
+        {
+            if(buttonA.Right-buttonA.Width >= 10)
+                buttonA.Left -= 10;
+            else if(buttonA.Right - buttonA.Width > 0)
+                buttonA.Left -= buttonA.Right;
+            if (this.ClientSize.Width-buttonB.Right >= 10)
+                buttonB.Left += 10;
+            else if (this.ClientSize.Width-buttonB.Right > 0)
+                buttonB.Left += this.ClientSize.Width-buttonB.Right;
+        }
+    }
+}
